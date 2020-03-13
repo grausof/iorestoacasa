74290b9720dd4c90ef8d68525e2a54be6799b897
Francesco <grausof@gmail.com>
2020-03-13 14:20:38 +0100

    Aggiunto modulo viaggio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { SpesaDomicilioComponent } from './spesa-domicilio/spesa-domicilio.component';
 import { CollaboraComponent } from './collabora/collabora.component';
 import { InserimentoAttivitaComponent } from './inserimento-attivita/inserimento-attivita.component';
-
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAnalyticsModule, ScreenTrackingService } from '@angular/fire/analytics';
-
-import { environment } from './../environments/environment';
-
-import { FormsModule } from "@angular/forms";
-
-
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAnalyticsModule, ScreenTrackingService } from '@angular/fire/analytics';
